d1cdf53c1bb31bfe3fb8f969de2529459fbc9f58
Ludmar <ludmar@ludmar-R490-G-BR52P1.(none)>
2014-03-10 21:07:03 -0300

    Correcao - Implementada a Classe - Dependecias

--- dependencias.py
+++ dependencias.py
@@ -1,7 +1,8 @@
 class Dependencias:
 	
-	def __init__(self, uiddependencias, nome, capacidade):
+	def __init__(self, uiddependencias, nome, capacidade, clube):
 		self.uiddependencias = uiddependencias
 		self.nome = nome
 		self.capacidade = capacidade
+		self.clube = clube
 
